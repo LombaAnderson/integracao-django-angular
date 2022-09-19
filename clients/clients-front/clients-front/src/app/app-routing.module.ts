2c757e95e586e11d817a08e34263394f57b710cd
Anderson Lomba <andersonlomba172@outlook.com>
2022-09-19 13:17:32 -0300

    New-Client component

--- clients/clients-front/clients-front/src/app/app-routing.module.ts
+++ clients/clients-front/clients-front/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientsDetailComponent } from './clients-detail/clients-detail.component';
+import { NewClientComponent } from './new-client/new-client.component';
 
 const routes: Routes = [
-  {path: 'client-detail/:id', component: ClientsDetailComponent}
+  {path: 'client-detail/:id', component: ClientsDetailComponent},
+  {path: 'new-client', component: NewClientComponent},
 ];
 
 @NgModule({
