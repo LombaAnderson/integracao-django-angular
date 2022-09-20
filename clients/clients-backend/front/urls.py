3234b2db8af17d0f050d23c2cbeff02ef89573b0
Anderson Lomba <andersonlomba172@outlook.com>
2022-09-20 13:28:23 -0300

    Template

--- clients/clients-backend/front/urls.py
+++ clients/clients-backend/front/urls.py
@@ -5,7 +5,8 @@ from core.views import ClientViewSet
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'clients', ClientViewSet)
@@ -23,10 +24,9 @@ schema_view = get_schema_view(
    permission_classes=[permissions.AllowAny],
 )
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include(router.urls) ),
     path('swagger', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
